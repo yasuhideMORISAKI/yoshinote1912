8f754f2ee49ac042fe3cd1f4b7708a1d82ae59d4
yasuhideMORISAKI <morisaki@i-pocket.co.jp>
2020-01-17 09:43:06 +0900

    カテゴリー一覧ページ／アカウント登録　ページ追加

--- js/font_size_btn.js
+++ js/font_size_btn.js
@@ -8,34 +8,38 @@ mediaQuery.addListener(handle);
 
 function handle(mq) {
   if (mq.matches) {
-  	// ウィンドウサイズが1027px以上のとき
-    $(function(){
+  // ウィンドウサイズが1027px以上のとき
+  $(function(){
+
   function initFontSize() {
     var size = (sessionStorage.getItem('fontSize'))? sessionStorage.getItem('fontSize') : '16';
     changeFontSize(size);
-  }
-  
-  function changeFontSize(size){
-    $('html').css('font-size', size + 'px');
-    $('[data-font!=' + size + ']').removeClass('active');
-    $('[data-font=' + size + ']').addClass('active');
-    sessionStorage.setItem('fontSize', size);
-  }
-  
-  function addListeners() {
-    $('[data-font]').on('click', function(){
+    }
+
+
+    function changeFontSize(size){
+      $('html').css('font-size', size + 'px');
+      $('[data-font!=' + size + ']').removeClass('active');
+      $('[data-font=' + size + ']').addClass('active');
+      sessionStorage.setItem('fontSize', size);
+    }
+
+    function addListeners() {
+      $('[data-font]').on('click', function(){
       changeFontSize($(this).data('font'));
-    });
-  }
-  
-  function init() {
-    initFontSize();
-    addListeners();
-  }
-  
-  init();
-});
-  } else {
-    // それ以外
+      });
+      }
+
+    function init() {
+      initFontSize();
+      addListeners();
+    }
+
+    init();
+      });
+    } 
+
+  else {
+  // それ以外
   }
 }
